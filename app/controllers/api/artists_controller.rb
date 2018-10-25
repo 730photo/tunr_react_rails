0641df6aabac6f6eee19d171e7a80bacbae0fa74
Reginald Lamar Bramber II <architekespy@Reginalds-MacBook-Pro.local>
2018-10-25 14:13:20 -0400

    first commit

---
+++ app/controllers/api/artists_controller.rb
@@ -0,0 +1,36 @@
+class Api::ArtistsController < ApplicationController
+   
+    def index
+    @artists = Artist.ArtistsController
+    render json: @artists
+   end
+   
+   def show
+    @artist = Artist.find(params[:id])
+    render json: @artist 
+   end
+
+   def create
+    @artist = Artist.create!(artist_params)
+    render json: @artist 
+   end
+
+   def update
+    @artist = Artist.find(params[:id])
+    @artist.update!(artist_params)
+    render json: @artist
+   end
+
+   def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    render status: 200
+   end
+
+
+   private 
+
+   def artist_params
+    params.require(:artist).permit(:name, :photo_url, :nationality)
+   end
+end
